34c310e0a4ad4b7ee59c54c2f1ebe36075927a6f
Giovanni Ramirez <jesussavedgr78@gmail.com>
2026-06-25 19:37:47 -0400

    added the firebase functionality and usersignup and login

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <StoreProvider>
-        <Auth/>
         <Navbar/>
         <body>{children}</body>
         <Footer/>
+        <Auth/>
       </StoreProvider>
     </html>
   );
--- components/auth.tsx
+++ components/auth.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { IoPersonSharp, IoCloseOutline } from "react-icons/io5";
 import googleLogo from "@/assets/google.png"
 import Image from "next/image";
@@ -7,52 +8,121 @@ import styles from "./auth.module.css"
 import type { RootState } from '@/store/store'; 
 import { useSelector, useDispatch } from 'react-redux';
 import { authModalToggle } from '@/store/slice';
+// firebase
+import React, { useState } from "react";
+import { auth } from "@/lib/firebase";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+
+interface LoginFormState {
+    email: string;
+    password: string;
+}
 
 export default function Auth() {
-    const isToggled = useSelector((state: RootState) => state.authModalToggle.status)
+    const [haveAccount, setHaveAccount] = useState<boolean>(true);
+    const [email, setEmail] = useState<LoginFormState["email"]>("");
+    const [password, setPassword] = useState<LoginFormState["password"]>("");
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log("Logged in:", userCredential.user);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            } else {
+                console.error(String(error));
+            }
+        }
+    };
+
+    const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log("Signed up:", userCredential.user);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            } else {
+                console.error(String(error));
+            }
+        }
+    };
+
+
+    const isToggled: boolean = useSelector((state: RootState) => state.authModalToggle.status)
     const dispatch = useDispatch()
 
 
     return (
         <>
-            {isToggled ?
-                <div className={styles.auth__wrapper} onClick={() => dispatch(authModalToggle())}>
-                    <div className={styles.auth} onClick={(e) => e.stopPropagation()}>
-                        <div className={styles.auth__content}>
-                            <div className={styles.auth__title}>Log in to Summarist</div>
-                            <button className={`${styles.btn} ${styles.guest__btn_wrapper}`}>
-                                <figure className={`${styles.auth__icon} ${styles.auth__icon_guest}`}>
-                                    <IoPersonSharp/>
-                                </figure>
-                                <div>Login as a guest</div>
-                            </button>
-                            <div className={styles.auth__separator}>
-                                <span className={styles.auth__separator_text}>or</span>
+            {isToggled ? (
+                haveAccount ? (
+                        <div className={styles.auth__wrapper} onClick={() => dispatch(authModalToggle())}>
+                            <div className={styles.auth} onClick={(e) => e.stopPropagation()}>
+                                <div className={styles.auth__content}>
+                                    <div className={styles.auth__title}>Log in to Summarist</div>
+                                    <button className={`${styles.btn} ${styles.guest__btn_wrapper}`}>
+                                        <figure className={`${styles.auth__icon} ${styles.auth__icon_guest}`}>
+                                            <IoPersonSharp/>
+                                        </figure>
+                                        <div>Login as a guest</div>
+                                    </button>
+                                    <div className={styles.auth__separator}>
+                                        <span className={styles.auth__separator_text}>or</span>
+                                    </div>
+                                    <button className={`${styles.btn} ${styles.google__btn_wrapper}`}>
+                                        <figure className={styles.auth__icon}>
+                                            <Image src={googleLogo} alt="" />
+                                        </figure>
+                                        <div>Login as a guest</div>
+                                    </button>
+                                    <div className={styles.auth__separator}>
+                                        <span className={styles.auth__separator_text}>or</span>
+                                    </div>
+                                    <form className={styles.auth__main_form} onSubmit={handleLogin}>
+                                        <input className={styles.auth__main_input} type="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)} />
+                                        <input className={styles.auth__main_input} type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                                        <button className="btn" type="submit">Login</button>
+                                    </form>
+                                </div>
+                                <div className={styles.auth__forgot_password}>Forgot your password?</div>
+                                <button className={styles.auth__switch_btn} onClick={() => setHaveAccount(false)}>Don&apos;t have an account?</button>
+                                <div className={styles.auth__close_btn} onClick={() => dispatch(authModalToggle())}>
+                                    <IoCloseOutline />
+                                </div>
                             </div>
-                            <button className={`${styles.btn} ${styles.google__btn_wrapper}`}>
-                                <figure className={styles.auth__icon}>
-                                    <Image src={googleLogo} alt="" />
-                                </figure>
-                                <div>Login as a guest</div>
-                            </button>
-                            <div className={styles.auth__separator}>
-                                <span className={styles.auth__separator_text}>or</span>
+                        </div>
+                    ) : (
+                        <div className={styles.auth__wrapper} onClick={() => dispatch(authModalToggle())}>
+                            <div className={styles.auth} onClick={(e) => e.stopPropagation()}>
+                                <div className={styles.auth__content}>
+                                    <div className={styles.auth__title}>Sign up to Summarist</div>
+                                    <button className={`${styles.btn} ${styles.google__btn_wrapper}`}>
+                                        <figure className={styles.auth__icon}>
+                                            <Image src={googleLogo} alt="" />
+                                        </figure>
+                                        <div>Sign up with Google</div>
+                                    </button>
+                                    <div className={styles.auth__separator}>
+                                        <span className={styles.auth__separator_text}>or</span>
+                                    </div>
+                                    <form className={styles.auth__main_form} onSubmit={handleSignup}>
+                                        <input className={styles.auth__main_input} type="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)} />
+                                        <input className={styles.auth__main_input} type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                                        <button className="btn" type="submit">Sign up</button>
+                                    </form>
+                                </div>
+                                <button className={styles.auth__switch_btn} onClick={() => setHaveAccount(true)}>Already have an account?</button>
+                                <div className={styles.auth__close_btn} onClick={() => dispatch(authModalToggle())}>
+                                    <IoCloseOutline />
+                                </div>
                             </div>
-                            <form className={styles.auth__main_form}>
-                                <input className={styles.auth__main_input} type="text" placeholder="Email Address"/>
-                                <input className={styles.auth__main_input} type="password" placeholder="Password"/>
-                                <button className="btn">login</button>
-                            </form>
                         </div>
-                        <div className={styles.auth__forgot_password}>Forgot your password?</div>
-                        <button className={styles.auth__switch_btn}>Don&apos;t have an account?</button>
-                        <div className={styles.auth__close_btn} onClick={() => dispatch(authModalToggle())}>
-                            <IoCloseOutline />
-                        </div>
-                    </div>
-                </div>
-            : 
-            <></>}
+                    )
+                ) : null}
         </>
     )
 }
