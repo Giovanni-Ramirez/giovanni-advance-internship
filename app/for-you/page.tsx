f62a50d4c5ceb263c70c83ceaf6bec24ec23f953
Giovanni Ramirez <jesussavedgr78@gmail.com>
2026-06-30 12:38:12 -0400

    finished the side bar with css

--- app/for-you/page.tsx
+++ app/for-you/page.tsx
@@ -1,6 +1,9 @@
+import SideNavbar from "@/components/sideNavBar";
+
 export default function ForYou() {
     return (
         <div>
+            <SideNavbar />
             <h1>This is the for you page</h1>
         </div>
     );
