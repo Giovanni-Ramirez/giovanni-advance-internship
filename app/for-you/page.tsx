3b91a60b1111f1989ff37f9f7621b9fa82ba0338
Giovanni Ramirez <jesussavedgr78@gmail.com>
2026-07-01 12:33:14 -0400

    finished the selected book section

--- app/for-you/page.tsx
+++ app/for-you/page.tsx
@@ -1,10 +1,38 @@
 import SideNavbar from "@/components/sideNavBar";
+import styles from './page.module.css';
+import SearchBar from '@/components/searchBar';
+import SelectedBook from '@/components/forYouComponents/selectedBook'
 
 export default function ForYou() {
+    
+    
+    
     return (
-        <div>
+        <div className={styles.right_border}>
             <SideNavbar />
-            <h1>This is the for you page</h1>
+            <div className={styles.search__background}>
+                <div className={styles.search__wrapper}>
+                    <div className={styles.search__sub_elem}></div>
+                    <SearchBar />
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="container">
+                    <section className="selected">
+                        <div className={styles.for_you__title}>Selected Just for you</div>
+                        <SelectedBook />
+                    </section>
+                    <section className="recommended">
+                        <div className={styles.for_you__title}> We think you&apos;ll like these</div>
+
+                    </section>
+                    <section className="suggested">
+                        <div className={styles.for_you__title}>Browse those books</div>
+
+                    </section>
+                </div>
+            </div>
         </div>
     );
 }
